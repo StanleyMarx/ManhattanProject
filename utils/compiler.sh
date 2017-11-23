7a3324fa7fc02deadfd38c5589363d1f4155fdc8
jonasblue <33932039+jonasblue@users.noreply.github.com>
2017-11-23 18:31:55 +0100

    Delete compiler.sh

--- utils/compiler.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-NAME=$(echo $1 | egrep -o '[A-Za-z0-9\ \_\-]+')
-gcc -I./ev3dev-c/source/ev3 -O2 -std=gnu99 -W -Wall -Wno-comment -c $1 -o $NAME.o
-gcc $NAME.o -Wall -lm -lev3dev-c -o $NAME
