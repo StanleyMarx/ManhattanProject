6847e8a1c156d55800352db823a475398f857c7b
JB <jb.durville@telecom-bretagne.eu>
2017-11-23 18:35:22 +0100

    minpr change to envoyer

--- utils/envoyer.sh
+++ utils/envoyer.sh
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+# usage: ./envoyer.sh file
+# action: will send file from Henri's laptop to the robot
 scp $1 robot@169.254.224.134:~/OS_Robot_Project_Fall2017/nosTest
-=======
-
->>>>>>> e5653faab711209c7c29fb6b2f19b748ba4ef314
