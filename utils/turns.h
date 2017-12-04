1567082f6117d3f8abb99aca143cd5aba4814d97
JB <jb.durville@telecom-bretagne.eu>
2017-12-04 16:22:01 +0100

    turns not done at all

---
+++ utils/turns.h
@@ -0,0 +1,8 @@
+#include "ev3.h"
+#include "ev3_port.h"
+#include "ev3_sensor.h"
+#include "ev3_tacho.h"
+
+extern void simpleTurn(int speed, int angle)
+extern void turnLeft()
+extern void turnRight()
