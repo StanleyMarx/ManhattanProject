a7bc16a9f70b3dc506682bd8806ef8226be9884b
JB <jb.durville@telecom-bretagne.eu>
2017-12-04 17:14:36 +0100

    moved to robotprogs

---
+++ robotprogs/turns.h
@@ -0,0 +1,7 @@
+#include "ev3.h"
+#include "ev3_port.h"
+#include "ev3_sensor.h"
+#include "ev3_tacho.h"
+
+extern void turnLeft(sn_left, sn_right, sn_gyro);
+extern void turnRight(sn_left, sn_right, sn_gyro);
