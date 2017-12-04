42546bf2cdca1059ac70cebd211618a00ae7d5bd
jonasblue <33932039+jonasblue@users.noreply.github.com>
2017-12-04 17:15:38 +0100

    Delete turns.h

--- utils/turns.h
+++
@@ -1,7 +0,0 @@
-#include "ev3.h"
-#include "ev3_port.h"
-#include "ev3_sensor.h"
-#include "ev3_tacho.h"
-
-extern void turnLeft(sn_left, sn_right, sn_gyro);
-extern void turnRight(sn_left, sn_right, sn_gyro);
