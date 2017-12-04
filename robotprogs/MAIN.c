da9ecded7792975881dcc42806dfcf50324d590c
JB <jb.durville@telecom-bretagne.eu>
2017-12-04 16:35:36 +0100

    first try MAIN.c

--- robotprogs/MAIN.c
+++ robotprogs/MAIN.c
@@ -17,8 +17,7 @@
 //////////////////////////////////////////////////
 #endif
 
-
-void find_sensors() {
+void findSensors() {
 	printf( "Found sensors:\n" );
 	for ( i = 0; i < DESC_LIMIT; i++ ) {
 		if ( ev3_sensor[ i ].type_inx != SENSOR_TYPE__NONE_ ) {
@@ -36,14 +35,87 @@ void find_sensors() {
 			}
 		}
 	}
+    	if (ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+		printf("COMPASS found, reading compass...\n");
+	 	if ( !get_sensor_value0(sn_compass, &value )) {
+			value = 0;
+		}
+		printf( "\r(%f) \n", value);
+		fflush( stdout );
+    	}
+	if (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+		printf("SONAR found, reading sonar...\n");
+		if ( !get_sensor_value0(sn_sonar, &value )) {
+			value = 0;
+		}
+		printf( "\r(%f) \n", value);
+		fflush( stdout );
+    	}
+	if (ev3_search_sensor(NXT_ANALOG, &sn_mag,0)){
+		printf("Magnetic sensor found, reading magnet...\n");
+		if ( !get_sensor_value0(sn_mag, &value )) {
+			value = 0;
+		}
+		printf( "\r(%f) \n", value);
+		fflush( stdout );
+    	}
 
 }
 
+void findMotors() {
+	// DEFINE LEFT_PORT, RIGHT_PORT and PELLE_PORT
+	// find left motor	
+	if ( ev3_search_tacho_plugged_in(LEFT_PORT,0, &sn_left, 0 )) {
+		printf( "LEGO_EV3_M_MOTOR LEFT is found\n" );
+	} else {
+		printf( "LEGO_EV3_M_MOTOR LEFT is NOT found\n" );
+	}
+	// find right one
+	if ( ev3_search_tacho_plugged_in(RIGHT_PORT,0, &sn_right, 0 )) {
+		printf( "LEGO_EV3_M_MOTOR RIGHT is found\n" );
+	} else {
+		printf( "LEGO_EV3_M_MOTOR RIGHT is NOT found\n" );
+	}
+	// find pelle motor
+	if ( ev3_search_tacho_plugged_in(PELLE_PORT,0, &sn_pelle, 0 )) {
+		printf( "LEGO_EV3_M_MOTOR PELLE is found\n" );
+	} else {
+		printf( "LEGO_EV3_M_MOTOR PELLE is NOT found\n" );
+	}
+}
 
+int main(void) {
+	uint8_t sn_left;
+	uint8_t sn_right;
+	uint8_t sn_pelle;
+	uint8_t sn_color;
+	uint8_t sn_compass;
+	uint8_t sn_sonar;
+	uint8_t sn_mag;
+	char s[ 256 ];
+	int val;
+	float value;
+#ifndef __ARM_ARCH_4T__
+	/* Disable auto-detection of the brick (you have to set the correct address below) */
+	ev3_brick_addr = "192.168.0.204";
 
+#endif
+	if ( ev3_init() == -1 ) return ( 1 );
 
+#ifndef __ARM_ARCH_4T__
+	printf( "The EV3 brick auto-detection is DISABLED,\nwaiting %s online with plugged tacho...\n", ev3_brick_addr );
 
+#else
+	printf( "Waiting tacho is plugged...\n" );
 
+#endif
+	while ( ev3_tacho_init() < 1 ) Sleep( 1000 );
 
-
+	printf( "*** ( EV3 ) Hello! ***\n" );
+	findMotors();	
+	ev3_sensor_init();
+	findSensors();
+	ev3_uninit();
+	printf( "*** ( EV3 ) Bye! ***\n" );
+	return ( 0 );
 }
