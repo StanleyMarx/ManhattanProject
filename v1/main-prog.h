ca0fc245842e5ab71c0304153361f305ec70f16c
StanleyMarx <33932009+StanleyMarx@users.noreply.github.com>
2018-01-08 15:45:07 +0100

    Update main-prog.h

--- v1/main-prog.h
+++ v1/main-prog.h
@@ -1,25 +1,37 @@
-int robot(){
-    // just test some sensors and tachos
-    
-    test_sensors_verbose();
-    test_tachos_verbose();
-
-    move_forever(50,50);
-    sleep(1);
-    move_forever(50,-50);
-    sleep(1);
-    move_forever(-50,50);
-    sleep(1);
-    move_forever(-50,-50);
-    sleep(1);
-    move_forever(0,0);
-    sleep(1);
-
-    move_real(210,-210,max_speed/2);
-    move_real(-210,210,max_speed/2);
-    move_real(800,800,max_speed/2);
-    move_real(-800,-800,max_speed/2);
-
-    open_shovel();
-    close_shovel();
+void* display_entry(){
+    int c,lval,rval;
+    printf("[THREAD] displaying the position value every second\n");
+    for (c=0; c<5; c++){
+        get_tacho_position(sn_rwheel,&rval);
+        get_tacho_position(sn_lwheel,&lval);
+        printf("[THREAD] R=%d,L=%d\n",rval,lval);
+        sleep(1);
+    }    
 }
+int test_update_pos(){
+    pthread_t display;
+    pthread_create(&display,NULL,display_entry,NULL);
+    
+    printf("[MAIN] forwarding for 5 seconds then stop\n");
+    move_forever(50,50);
+    sleep(5);
+    move_forever(0,0);
+    
+    pthread_join(display,NULL);
+    return 0;
+}
+
+int X=0,Y=0;
+
+int robot(int sw){
+    switch (sw){
+        case 0:
+            test_update_pos();
+            break;
+    }
+}
+
+/*
+    0
+        test sur l'update des positions
+*/
