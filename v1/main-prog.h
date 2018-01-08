64ebc4e7c15f782b552e79e940a5806627614bd4
StanleyMarx <33932009+StanleyMarx@users.noreply.github.com>
2018-01-08 17:19:56 +0100

    Update main-prog.h

--- v1/main-prog.h
+++ v1/main-prog.h
@@ -2,10 +2,10 @@ void* display_entry(){
     int c,lval,rval;
     printf("[THREAD] displaying the position value every second\n");
     for (c=0; c<5; c++){
+        sleep(1);
         get_tacho_position(sn_rwheel,&rval);
         get_tacho_position(sn_lwheel,&lval);
         printf("[THREAD] R=%d,L=%d\n",rval,lval);
-        sleep(1);
     }    
 }
 int test_update_pos(){
