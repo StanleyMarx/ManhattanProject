2824ac18eed17f68565b61b060068d015c915e89
JB <jb.durville@telecom-bretagne.eu>
2018-01-08 13:49:46 +0100

    deleted turn.c and avancer.c

--- robotprogs/turns.h
+++
@@ -1,7 +0,0 @@
-#include "ev3.h"
-#include "ev3_port.h"
-#include "ev3_sensor.h"
-#include "ev3_tacho.h"
-
-extern void turnLeft(uint8_t sn_left, uint8_t sn_right, uint8_t sn_gyro);
-extern void turnRight(uint8_t sn_left, uint8_t sn_right, uint8_t sn_gyro);
