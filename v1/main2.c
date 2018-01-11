7b521424d0543a4c26146f6eeaa3b43a078455a6
JB <jb.durville@telecom-bretagne.eu>
2018-01-11 13:50:15 +0100

    main2

--- v1/main2.c
+++ v1/main2.c
@@ -68,47 +68,8 @@ int main(int argc, char **argv) {
     ev3_search_tacho_plugged_in(PORT_SHOVEL,0,&sn_shovel,0);
     ev3_search_tacho_plugged_in(PORT_LEFTWHEEL,0,&sn_lwheel,0);
     get_tacho_max_speed(sn_rwheel,&max_speed);
-    
-    
-  struct sockaddr_rc addr = { 0 };
-  int status;
 
-  /* allocate a socket */
-  s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-
-  /* set the connection parameters (who to connect to) */
-  addr.rc_family = AF_BLUETOOTH;
-  addr.rc_channel = (uint8_t) 1;
-  str2ba (SERV_ADDR, &addr.rc_bdaddr);
-
-  /* connect to server */
-  status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
-
-  /* if connected */
-  /*if( status == 0 ) {
-    char string[58];
-
-    /* Wait for START message */
-    /*read_from_server (s, string, 9);
-    if (string[4] == MSG_START) {
-      printf ("Received start message!\n");
-    }
-      
-        /* ROBOT */
-	*/      
 	robot(atoi(argv[1]));
-    /*  
-      
-    close (s);
-
-    sleep (5);
 	
-  } else {
-    fprintf (stderr, "Failed to connect to server...\n");
-    sleep (2);
-    exit (EXIT_FAILURE);
-  }
-
-  close(s);*/
-  return 0;
+	return 0;
 }
