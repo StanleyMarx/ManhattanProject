56a748c9b5e3add66aa83e7d836f6a13c8d446e5
hen700 <henri.aynie@orange.fr>
2018-01-11 15:30:36 +0100

    Update main-prog.h

--- v1/main-prog.h
+++ v1/main-prog.h
@@ -87,11 +87,11 @@ int test_Update_position(){
     forward_sonar(200,200,200);*/
     
     
-    turn_exact_abs(0,3);
+    
     move_forever(50,50);
     sleep(5);
     move_forever(0,0);
-    turn_exact_abs(90,3);
+    turn_exact_rel(90,3);
     move_forever(50,50);
     sleep(5);
     move_forever(0,0);
