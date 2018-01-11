7579a7beaeefb7ed335370dad444e695f73ba45d
hen700 <henri.aynie@orange.fr>
2018-01-11 14:24:20 +0100

    Update main-prog.h

--- v1/main-prog.h
+++ v1/main-prog.h
@@ -1,3 +1,4 @@
+//--------------------------- CASE_0 ---------------------------
 void* display_entry(){
     int c,lval,rval;
     printf("[THREAD] displaying the position value every second\n");
@@ -21,11 +22,12 @@ int test_update_pos(){
     return 0;
 }
 
+//--------------------------- CASE_1 ---------------------------
 int pi=3.14159265;
 int Xdef=0,Ydef=0;
 int speedMotorL, speedMotorR, positionMotorL1, positionMotorR1, positionMotorL2, positionMotorR2;
 float thetaCompas;
-float lambda=21.21;
+float lambda=1/21.21;
 int ThreadDisplay=0;
 pthread_mutex_t lock;
 
@@ -45,20 +47,23 @@ void* Update_position(){
                 get_tacho_position(sn_lwheel, &positionMotorL2);
                 get_tacho_position(sn_rwheel, &positionMotorR2);
                 get_sensor_value0(sn_compass, &thetaCompas);
-
-                if ((speedMotorR != 0) && (speedMotorL != 0)) {
+		
+                printf("\n speedMotorL,speedMotorR = %d,%d\n",speedMotorL,speedMotorR);
+                /*
+		if ((speedMotorR != 0) && (speedMotorL != 0)) {
 			if (speedMotorL/speedMotorR > 0) {
 				printf("\nrobot is moving");
-				printf("\nsin(thetaCompas)",sin(thetaCompas));
-				printf("\ndiffPosition",positionMotorR2-positionMotorR1);
-				printf("\nlambda",lambda);
+				printf("\nsin(thetaCompas) %F",sin(thetaCompas));
+				printf("\ndiffPosition %d",positionMotorR2-positionMotorR1);
+				printf("\nlambda %F",lambda);
 				Xdef=Xdef-sin(thetaCompas)*(positionMotorR2-positionMotorR1)*lambda;
 				Ydef=Ydef+cos(thetaCompas)*(positionMotorR2-positionMotorR1)*lambda;
 			} else {
 				printf("\nrobot is turning");
 			}
 		}
-                printf("x,y = %d,%d\n",Xdef,Ydef);
+                printf("\n x,y = %d,%d\n",Xdef,Ydef);
+		*/
 
                 /* debut SC1 */
                 pthread_mutex_lock(&lock);
@@ -99,6 +104,7 @@ int test_Update_position(){
     return 0;
 }
 
+//--------------------------- CASE_2 ---------------------------
 void test_cs(){
     send_position(1,2);
     send_position(-1,-2);
@@ -109,6 +115,7 @@ void test_cs(){
 
 int X=0,Y=0;
 
+//--------------------------- ROBOT ---------------------------
 int robot(int sw){
     switch (sw){
         case 0:
@@ -121,14 +128,13 @@ int robot(int sw){
             test_cs();
             break;
         case 3: // test sensor JB
-        	forward_sonar(50, 50, 40.0, 8000, 20);
+        	forward_sonar(50, 50, 80.0, 8000, 20);
 		if (detect_movable() == 1){
-			printf("movable\n");
+			printf("movable");
 		}
 		else{
-			printf("non movable\n");
+			printf("non movable");
 		}
-		forward_sonar(50, 50, 60.0, 2000, 20);
         	
     }
 	
