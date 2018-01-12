bc98f8c8c4a85d46688c34da0283ce450656af41
JB <jb.durville@telecom-bretagne.eu>
2018-01-12 17:45:51 +0100

    updated main.c

--- v1/main.c
+++ v1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <time.h>
@@ -64,11 +65,16 @@ int main(int argc, char **argv) {
     ev3_sensor_init();
     ev3_search_sensor(HT_NXT_COMPASS,&sn_compass,0);
     ev3_search_sensor(LEGO_EV3_US,&sn_sonar,0);
+    ev3_search_sensor(LEGO_EV3_COLOR,&sn_color,0);
+   	set_sensor_mode(sn_color, "COL-COLOR");
     ev3_search_tacho_plugged_in(PORT_RIGHTWHEEL,0,&sn_rwheel,0);
     ev3_search_tacho_plugged_in(PORT_SHOVEL,0,&sn_shovel,0);
     ev3_search_tacho_plugged_in(PORT_LEFTWHEEL,0,&sn_lwheel,0);
     get_tacho_max_speed(sn_rwheel,&max_speed);
     
+    // empty the Position pos.txt file where we'll store all of our coordinates
+    posFile = fopen("pos.txt", "w");
+	fclose(posFile);
     
   struct sockaddr_rc addr = { 0 };
   int status;
