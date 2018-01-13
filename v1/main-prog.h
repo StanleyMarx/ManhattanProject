cc582cf9e19e3077aa3d8049bf2166c58889fd9a
tarentule <33935168+tarentule@users.noreply.github.com>
2018-01-13 16:58:31 +0100

    Update main-prog.h

--- v1/main-prog.h
+++ v1/main-prog.h
@@ -75,6 +75,7 @@ int robot(int sw,int arg1,int arg2){
             test_update_pos();
             break;
         case 1:
+	    test_Update_position2();
 	    forward_sonar(50, 50, 50, 10000, 20);
 	    detect_type(50);
          
