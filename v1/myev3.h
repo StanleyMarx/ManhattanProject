21df661f10538eeea0c6438e8fbc863a389b27f3
tarentule <33935168+tarentule@users.noreply.github.com>
2018-01-14 15:10:07 +0100

    Update myev3.h

--- v1/myev3.h
+++ v1/myev3.h
@@ -901,6 +901,7 @@ int detect_type(int sonarThreshold){
 	int i = 0;
 	printf(" pos %d %d \n", a, b);
 	while (i<10){ 
+		print("i = %d",i);
 		i = i + 1;
 		printf("in1");
 		while (sonarVal < sonarThreshold) {
